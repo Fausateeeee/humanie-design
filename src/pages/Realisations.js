9490581f595cc4ff0428457cec561cd7faef5042
Charles-David Lachance <charles-david.lachance@fausa.tech>
2020-09-15 18:16:19 -0400

    New realisations component

--- src/pages/Realisations.js
+++ src/pages/Realisations.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
-// import { Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap'
+import { Row, Col, Container } from 'reactstrap'
+import HeaderPicture from '../components/HeaderPicture'
+import ProjectThumbnail from '../components/ProjectThumbnail'
+import Project from '../components/Project'
+import PROJECTS from '../data/projects'
+
+const ProjectsNavigator = () => {
+  return (
+    <div className='p-3 p-md-5' id='realisations'>
+      <h1 className='text-center display-4'>Projets résidentiels</h1>
+      <Row>
+        <Col>
+          <ProjectThumbnail project={PROJECTS[0]} />
+        </Col>
+        <Col>
+          <ProjectThumbnail project={PROJECTS[1]} />
+        </Col>
+      </Row>
+    </div>)
+}
+
+const ProjectExplorer = ({ id }) => {
+  const project = PROJECTS.filter((el) => { return el.id == id })[0]
+  return <Project project={project} />
+}
 
 class Realisations extends Component {
   render () {
+    console.log(this.props.match.params.projectId)
+
     return (
-      <div>
-        <h1>This is realisations</h1>
-      </div>
+      <Container>
+        <HeaderPicture path='/assets/headers/realisations.jpg' />
+        {this.props.match.params.projectId ? <ProjectExplorer id={this.props.match.params.projectId} /> : <ProjectsNavigator />}
+      </Container>
     )
   }
 }
