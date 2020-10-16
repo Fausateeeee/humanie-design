c004f8c8647a1bfac5665f43af6ecfe7f2513fdc
Charles-David Lachance <charles-david.lachance@fausa.tech>
2020-10-15 23:54:41 -0400

    Updated the component to render only if a path is given

--- src/components/HeaderPicture.js
+++ src/components/HeaderPicture.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const HeaderPicture = ({ path }) => {
-  return (
+export const HeaderPicture = ({ path }) => {
+  const picture = (
     <div className='row flex-nowrap justify-content-between align-items-center mt-3'>
       <img src={path} className='img-fluid' alt='header' />
     </div>
+  )
 
+  return (
+    path ? picture : <div />
   )
 }
 
 HeaderPicture.propTypes = {
-  path: PropTypes.string.isRequired
+  path: PropTypes.string
 }
-
-export default HeaderPicture
