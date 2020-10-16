2cf0e839561a16cfcc1fc87bb7b90960fa07ae8d
Charles-David Lachance <charles-david.lachance@fausa.tech>
2020-10-15 21:41:14 -0400

    Created index where all the exports are done

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import HomePage from './Home'
+import AboutPage from './About'
+import ContactPage from './Contact'
+import RealisationsPage from './Realisations'
+import ServicesPage from './Services'
+import Error404Page from './Error404'
+
+export {
+  HomePage,
+  AboutPage,
+  ContactPage,
+  RealisationsPage,
+  ServicesPage,
+  Error404Page
+}
